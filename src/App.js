1922a0ac2bb721f8273ed01da250882aae0e761b
Moss <charlesiddle99@gmail.com>
2022-07-17 11:31:35 +0300

    finished done!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Footer from './components/footer/Footer';
+import Nav1 from './components/Nav/Nav1';
+import Nav2 from './components/Nav/Nav2';
+import Homepage from './views/Homepage/Homepage';
+import Splashscreen from './views/Splashscreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <Switch>
+        <Route path="/" exact render={(props) => <HomePage {...props} />} />
+        </Switch> */}
+      <Splashscreen />
+      <Nav1 />
+      <Nav2 />
+      <Switch>
+        <Route path='/' exact render={(props) => <Homepage {...props} />} />
+      </Switch>
+      <Footer />
+      {/* 
+      Nav1
+       Nav2
+       Banner
+       ForexSlider
+       SolutionsForYou
+       SolutionsForBusiness
+       EliteBanking
+       Loan
+       BankingTools
+       CallToAction
+       Footer
+       */}
     </div>
   );
 }
