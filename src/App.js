9c7147c2e3ef48ca4eb54e15f93dcdb646306a6a
Moss <charlesiddle99@gmail.com>
2022-07-17 11:51:52 +0300

    removed router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 import Footer from './components/footer/Footer';
 import Nav1 from './components/Nav/Nav1';
 import Nav2 from './components/Nav/Nav2';
-import Homepage from './views/Homepage/Homepage';
+import Apps from './views/Homepage/Apps';
+import Banner from './views/Homepage/Banner';
+import BusinessSolutions from './views/Homepage/BusinessSolutions';
+import EliteBanking from './views/Homepage/EliteBanking';
+import LoanCalculator from './views/Homepage/LoanCalculator';
+import Solutions from './views/Homepage/Solutions';
+import Ticker from './views/Homepage/Ticker';
+import Tools from './views/Homepage/Tools';
+// import Homepage from './views/Homepage/Homepage';
+// import Banner from './Banner';
+// import Ticker from './Ticker';
+// import Solutions from './Solutions';
+// import BusinessSolutions from './BusinessSolutions';
+// import EliteBanking from './EliteBanking';
+// import LoanCalculator from './LoanCalculator';
+// import Tools from './Tools';
+// import Apps from './Apps';
 import Splashscreen from './views/Splashscreen';
 
 function App() {
@@ -15,9 +31,17 @@ function App() {
       <Splashscreen />
       <Nav1 />
       <Nav2 />
-      <Switch>
+      {/* <Switch>
         <Route path='/' exact render={(props) => <Homepage {...props} />} />
-      </Switch>
+      </Switch> */}
+      <Banner />
+      <Ticker />
+      <Solutions />
+      <BusinessSolutions />
+      <EliteBanking />
+      <LoanCalculator />
+      <Tools />
+      <Apps />
       <Footer />
       {/* 
       Nav1
